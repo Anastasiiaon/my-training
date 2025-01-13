78d877e174671d2249b41f9048b4711e19df1fc8
Anastasiiaon <nastya.silaeva.1997@mail.ru>
2025-01-13 17:42:40 +1000

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,5 @@
+how_homework = 12
+how_time = 1.5
+name = "Pithon"
+mine_time = (how_time / how_homework)
+print("Курс:", name, "; " "всего задач:", how_homework, "; " "затрачено часов:", how_time, "; " "среднее время выполнения", mine_time)
